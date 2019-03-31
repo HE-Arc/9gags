0997926e9f28ce3b25f7e6a61219fbbfd0da4736
Steven Jeanneret <steven.jeanneret@he-arc.ch>
2019-03-31 22:20:01 +0200

    add navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
+    <Navbar />
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/includes/Navbar'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Navbar,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ frontend/src/components/includes/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-navbar toggleable="md" type="dark" variant="dark">
+      <b-navbar-brand href="/">NavBar</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#">Link2</b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="/login">Login</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar'
+  }
+</script>
+
+<style scoped>
+
+</style>
