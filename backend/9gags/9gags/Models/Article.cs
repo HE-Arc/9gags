3a654b4e7e3aaf6433012ca9bf6b90396b962c11
Charles-Lewis jaggi <charles-lewis.jaggi@he-arc.ch>
2019-04-01 14:47:34 +0200

    models and context

---
+++ backend/9gags/9gags/Models/Article.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _9gags.Models
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Path { get; set; }
+        public int points { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public List<Vote> Votes { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<View> Views { get; set; }
+
+    }
+}
+
---
+++ backend/9gags/9gags/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _9gags.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string email { get; set; }
+        public string username { get; set; }
+
+        public List<Vote> Votes { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<View> Views { get; set; }
+    }
+}
