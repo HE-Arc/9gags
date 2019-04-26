66f1e5848e3a8d2a7ed581e6cb8211d27b79d4da
Charles-Lewis jaggi <charles-lewis.jaggi@he-arc.ch>
2019-04-26 16:53:37 +0200

    news

---
+++ backend/9gags/9gags/Controllers/NewsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
+using _9gags.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _9gags.Controllers
+{
+    [Route("api/[controller]")]
+    public class NewsController : ControllerBase
+    {
+        #region initalisation 
+        private readonly GagsContext _context;
+        public static IHostingEnvironment _environment;
+        public NewsController(IHostingEnvironment environment, GagsContext context)
+        {
+            _environment = environment;
+            _context = context;
+        }
+        #endregion
+
+        #region get image news
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Article>>> GeArticles()
+        {
+            
+            return await _context.Articles.OrderByDescending(s => s.ReleaseDate).ToListAsync();
+        }
+        #endregion
+
+    }
+}
