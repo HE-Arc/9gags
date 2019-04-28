6b041c34a086f002e8b64a1092306ed3080d9851
Steven Jeanneret <steven.jeanneret@outlook.com>
2019-04-28 23:04:25 +0200

    home page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,25 +1,41 @@
 <template>
-  <div>
-    <h1>This is a home page</h1>
-    <button @click="makeReq">Request</button>
-    {{msg}}
+  <div class="container">
+    <h1 class="display-3 text-center mb-4">Welcome on 9gags</h1>
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6 col-12 my-md-4">
+          <div class="card text-white bg-light mb-3">
+            <div class="card-header bg-dark text-center">View memes</div>
+            <div class="card-body text-dark">
+              <img src="@/assets/1.png" class="img-fluid" alt="">
+              <p class="card-text">See the best memes only once !</p>
+            </div>
+          </div>
+        </div>
+    <div class="col-md-6 col-12">
+      <div class="card text-white bg-light mb-3">
+            <div class="card-header bg-dark text-center">Vote for the best memes</div>
+            <div class="card-body text-dark text-center">
+              <img src="@/assets/2.png" class="img-fluid" alt="">
+              <p class="card-text text-left">Vote for your favourite memes !</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-12">
+          <div class="card text-white bg-light mb-3">
+            <div class="card-header bg-dark text-center">Share your advice with other people</div>
+            <div class="card-body text-dark">
+              <img src="@/assets/3.png" class="img-fluid" alt="">
+              <p class="card-text">Comment posts to share your advice with other people !</p>
+            </div>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
   export default {
     name: 'Home',
-    data () {
-      return {
-        msg: ''
-      }
-    },
-    methods: {
-      makeReq () {
-        this.axios.get('https://localhost:44342/api/values').then(result => {
-          this.msg = result.data
-        })
-      }
-    }
   }
 </script>
